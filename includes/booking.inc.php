4e0d224bde376d0e02e7214731f90ac53a0c040e
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2020-04-25 22:04:24 +0400

    update

--- includes/booking.inc.php
+++ includes/booking.inc.php
@@ -9,11 +9,9 @@
      $roomType = $_POST['roomType'];
      $paymentType = $_POST['payment-type'];
      $roomNumber = $_POST['roomNumber'];
-     $numOfDays = $_POST['numDays1'];
-     $bill = $_POST['numDays'];
 
 
-     $sql = "INSERT INTO booking (uidUsers, checkinDate, checkoutDate, roomType, paymentType, roomNumber, numOfDays, bill) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+     $sql = "INSERT INTO booking (uidUsers, checkinDate, checkoutDate, roomType, paymentType, roomNumber) VALUES (?, ?, ?, ?, ?, ?)";
      $stmt = mysqli_stmt_init($conn);
 
      if(!mysqli_stmt_prepare($stmt, $sql)) {
@@ -29,7 +27,7 @@
        }
 
        else {
-       mysqli_stmt_bind_param($stmt, "ssssssss", $username, $checkinDate, $checkoutDate, $roomType, $paymentType, $roomNumber, $numOfDays, $bill);
+       mysqli_stmt_bind_param($stmt, "ssssss", $username, $checkinDate, $checkoutDate, $roomType, $paymentType, $roomNumber);
        mysqli_stmt_execute($stmt);
        // mysqli_stmt_store_result($stmt); for fecthing from database purposes
        header("Location: ../booking.php?booking=success");
